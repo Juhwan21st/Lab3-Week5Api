d6f31946405486f4d1dc562c7b9555c1b25dc64f
Juhwan21st <jseo9123@conestogac.on.ca>
2026-02-14 23:54:45 -0500

    Enable Swagger in production for Azure testing

--- Program.cs
+++ Program.cs
@@ -20,7 +20,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Enable Swagger in production for Azure testing
 app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
